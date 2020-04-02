b0525efde8dd08f0dab2b1f1ded5b5cbfe983fa0
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-04-02 08:22:38 +0700

    Initial bounch ball feature

--- main.c
+++ main.c
@@ -3,6 +3,29 @@
 
 #use delay(clock=4M)
 
+
+void bounchBall() {
+   int8 x = 0;
+   int8 y = 1;
+   
+   int8 xVector = 1;
+   int8 yVector = 1;
+
+   while (1) {
+      createBall(x, y);
+      clearBall(x, y);
+      
+      // calculate next position
+      if (x == 15) xVector = -1;
+      if (x == 0) xVector = 1;
+      x += xVector;
+      
+      if (y == 14) yVector = -1;
+      if (y == 1) yVector = 1;
+      y += yVector;
+   }
+}
+
 void main() {
    initialTRIS();
    initialBoard();
@@ -14,10 +37,12 @@ void main() {
    // wait for intial glcd module
    delay_us(1000);
    
-   while(1) {
-      for(int i = 0; i < 16; i++) {
-         createBall(i, i);
-         clearBall(i, i);
-      }
-   }
+   bounchBall();
+   
+//!   while(1) {
+//!      for(int i = 0; i < 16; i++) {
+//!         createBall(i, i);
+//!         clearBall(i, i);
+//!      }
+//!   }
 }
