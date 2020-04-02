e2bef4dfa29f584d175b3db06d984bdcf1d9c986
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-04-02 21:42:00 +0700

    Update render bounch ball in main

--- main.c
+++ main.c
@@ -20,26 +20,30 @@
 #use delay(clock=4M)
 
 
-void bounchBall(startX, startY, endX, endY) {
-   int8 x = startX;
-   int8 y = startY;
-   
-   int8 xVector = 1;
-   int8 yVector = 1;
+const int8 startX = 0;
+const int8 startY = 1;
+const int8 endX = 15;
+const int8 endY = 14;
 
-   while (1) {
-      createBall(x, y);
-      clearBall(x, y);
-      
-      // calculate next position
-      if (x == endX) xVector = -1;
-      if (x == startX) xVector = 1;
-      x += xVector;
-      
-      if (y == endY) yVector = -1;
-      if (y == startY) yVector = 1;
-      y += yVector;
-   }
+int8 x = startX;
+int8 y = startY;
+int8 xVector = 1;
+int8 yVector = 1;
+
+void bounchBall() {
+
+   createBall(x, y);
+   clearBall(x, y);
+   
+   // calculate next position
+   if (x == endX) xVector = -1;
+   if (x == startX) xVector = 1;
+   x += xVector;
+   
+   if (y == endY) yVector = -1;
+   if (y == startY) yVector = 1;
+   y += yVector;
+
 }
 
 void main() {
@@ -53,12 +57,7 @@ void main() {
    // wait for intial glcd module
    delay_us(1000);
    
-   bounchBall(0, 1, 15, 14);
-   
-//!   while(1) {
-//!      for(int i = 0; i < 16; i++) {
-//!         createBall(i, i);
-//!         clearBall(i, i);
-//!      }
-//!   }
+   while(1) {
+      bounchBall();
+   }
 }
